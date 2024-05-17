8155e32f63175042758b4af2660f06eea166033d
Giovanny van Kerckhove <gio_v_k@hotmail.com>
2024-05-17 14:39:05 +0200

    First commit

---
+++ paris2024-Back/src/main/java/com/example/paris2024Back/Paris2024BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.paris2024Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paris2024BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paris2024BackApplication.class, args);
+	}
+
+}
---
+++ paris2024-Back/src/test/java/com/example/paris2024Back/Paris2024BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paris2024Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paris2024BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
